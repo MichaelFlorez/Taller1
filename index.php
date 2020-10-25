f9eb513058087869edeaef2a30e1f7ba18c12707
Michael Florez Rico <michael.florez.r@gmail.com>
2020-10-25 15:40:01 -0600

    se agrega texto e imagenes

---
+++ index.php
@@ -0,0 +1,65 @@
+<DOCTYPE html>
+
+
+
+<html>
+    <head>
+        <title>
+            Agujeros negros
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    </head>
+    <body>
+        
+        <br>
+        <br>
+        <h1>¿Qué es un agujero negro?</h1>
+        <br>
+        <p>Un agujero negro1​ es una región finita del espacio en cuyo interior existe una concentración de masa lo suficientemente elevada como para generar un <br>campo gravitatorio tal que ninguna partícula material, ni siquiera la luz, puede escapar de ella. Sin embargo, los agujeros negros pueden ser capaces de <br>emitir un tipo de radiación, la radiación de Hawking, conjeturada por Stephen Hawking en la década de 1970. La radiación emitida por agujeros negros<br>como Cygnus X-1 no procede del propio agujero negro sino de su disco de acreción.</p>
+        <br>
+        <img id=image1 src="image/AN1.jpg">
+        <br>
+        <p>La gravedad de un agujero negro, o «curvatura del espacio-tiempo», provoca una singularidad envuelta por una superficie cerrada, llamada horizonte <br>de eventos. Esto es previsto por las ecuaciones del campo de Einstein. El horizonte de sucesos separa la región del agujero negro del resto del universo,<br> y a partir de él ninguna partícula puede salir, incluidos los fotones. Dicha curvatura es estudiada por la relatividad general, la que predijo la existencia<br> de los agujeros negros y fue su primer indicio. </p>
+        <br>
+
+        <h2>Radio de Schwarzschild</h2>
+        <br>
+        <p>El radio de Schwarzschild es la medida del tamaño de un agujero negro de Schwarzschild, es decir, un agujero negro de simetría esférica y estático. Se <br>corresponde con el radio aparente del horizonte de sucesos, expresado en coordenadas de Schwarzschild.</p>
+        <br>
+        <p>Puesto que el tamaño de un agujero negro depende de la energía absorbida por el mismo, cuanto mayor es la masa del agujero negro, tanto mayor es el <br>radio de Schwarzschild, que viene dada por:
+        </p>
+        <br>
+        <img id=image2 src="image/AN2.png">
+        <br>
+        <ul id=list>
+            Donde:
+            <li>G es la constante gravitatoria</li>
+            <li>M es la masa del objeto</li>
+            <li>c es la velocidad de la luz</li>
+        </ul>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>        
+        <br>
+        <br>
+        <br>  
+        <br>
+        <br>  
+        <br>
+        <br> 
+        <h2>Referencias</h2>
+      
+        <ul id=list2>
+           
+            <li><a href= https://es.wikipedia.org/wiki/Agujero_negro#:~:text=El%20horizonte%20de%20sucesos%20separa,y%20fue%20su%20primer%20indicio.>Agujero negro</a></li>
+            <li><a href=https://es.wikipedia.org/wiki/Radio_de_Schwarzschild>Radio de Schwarzschild</a></li>
+
+            
+        </ul>
+
+    </body>
+</html>
